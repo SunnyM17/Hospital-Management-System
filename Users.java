e512d7181b9e925a8a21932919fa654e722d6da1
Sunny Mangat <guransh.mangat1@ucalgary.ca>
2020-03-03 10:39:43 -0700

    Created getters and setters

--- Users.java
+++ Users.java
@@ -1,18 +1,64 @@
+import java.io.IOException;
+
 public class Users
 {
-
+    private String username;
+    private String password;
+    private String userID;  
     public Users()
     {
 
     }
-    private String username;
-    private String password;
-    private String userID;
+
     public Users(String userID, String username, String passwords)
     
     {
-        this.userID = userID;
-        this.username = username;
-        this.password = password;
+        setUserID(userID);
+        setUsername(username);
+        setPassword(password);
     }
+
+    public String getUserID(String userID)
+    {
+        return userID;
+    }
+
+    public String getUsername(String username)
+    {
+        return username;
+    }
+    public String getPassword(String password)
+    {
+        return password;
+    }
+
+
+    public void setUserID(String userID)
+    {
+        this.userID = userID;
+    }
+
+    public void setUsername(String username)
+    
+    {
+        if (username.length() < 12)
+        {
+            this.username = username;
+        }
+        
+    
+    }
+
+    public void setPassword(String password)
+    
+    {
+        if (passowrd.length() > 8)
+        {
+            this.password = password;
+        }
+        throw IOException("Password needs to be atleast 8 characters long.");
+        
+    }
+
+
 }
