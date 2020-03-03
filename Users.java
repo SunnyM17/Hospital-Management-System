d793b71571b5279da5c09c32891d996e1bf45ac6
Sunny Mangat <guransh.mangat1@ucalgary.ca>
2020-03-03 10:32:17 -0700

    Created User.java
    Created constructors

---
+++ Users.java
@@ -0,0 +1,18 @@
+public class Users
+{
+
+    public Users()
+    {
+
+    }
+    private String username;
+    private String password;
+    private String userID;
+    public Users(String userID, String username, String passwords)
+    
+    {
+        this.userID = userID;
+        this.username = username;
+        this.password = password;
+    }
+}
